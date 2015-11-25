b7c10cbf602bdd00d42dfd5cb677f7cc7130c4dd
isabo <isabo@claritude.com>
2015-11-25 15:50:18 +0200

    Annotate externs file (though it does not seem supported)

--- src/externs.js
+++ src/externs.js
@@ -1,4 +1,9 @@
-// module.exports is not defined in Google Closure Compiler's node externs, so we define it here.
+/**
+ * module.exports is not defined in Google Closure Compiler's node externs, so we define it here.
+ *
+ * @externs
+ */
+ 
 var module = {
     exports: {}
 }
